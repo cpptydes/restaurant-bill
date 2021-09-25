1c605961079a3cc5d6e5bf2c347688f6847814eb
Tyler Kloss <tkloss@cpp.edu>
2021-09-24 23:29:20 -0700

    Outlining guessing game program

---
+++ guessingGame.c
@@ -0,0 +1,21 @@
+// insert libraries
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+// main
+int main() {
+    //switch statements for the following cases
+
+    //press 1 to play game
+        //prompt user to enter number to play game
+        //make sure to check for q input to return to menu
+    
+    //press 2 to change the max number
+
+    //press 3 to quit
+
+
+
+    return 0;
+}
