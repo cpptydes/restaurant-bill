d44e3e773178384c8de14fa54b1e78a09c4a9504
Tyler Kloss <tkloss@cpp.edu>
2021-09-24 23:15:51 -0700

    Initial commit outlining program

---
+++ restaurantBill.c
@@ -0,0 +1,18 @@
+// insert libraries
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+// main
+int main( int argc, char *argv[] ) {
+    //set floating point precision
+
+    //generate random meal number
+
+    //...
+
+
+
+    return 0;
+}
+
